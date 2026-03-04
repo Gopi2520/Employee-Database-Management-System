1e4c06749b3be3a9dd0676b34012c5c88a6e9228
Gopi <gopi27644@gmail.com>
2026-03-04 13:37:15 +0530

    error fixed

--- src/main/java/com/example/login_demo/LoginController.java
+++ src/main/java/com/example/login_demo/LoginController.java
@@ -1,16 +1,38 @@
 package com.example.login_demo;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class LoginController {
-    @PostMapping("/login")
-    @CrossOrigin(origins = "*")
 
-    public Map<String, Object> login(@RequestBody User user) {
+    @PostMapping(
+            value = "/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @CrossOrigin(origins = "*")
+    public Map<String, Object> loginJson(@RequestBody User user) {
+        return doLogin(user);
+    }
+
+    @PostMapping(
+            value = "/login",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @CrossOrigin(origins = "*")
+    public Map<String, Object> loginForm(User user) {
+        return doLogin(user);
+    }
+
+    private Map<String, Object> doLogin(User user) {
         Map<String, Object> response = new HashMap<>();
         if ("we".equals(user.getUsername()) && "we".equals(user.getPassword())) {
             response.put("success", true);
