978e3946a4580206a84794be1b45a5c35fb3ce38
Gopi <gopi27644@gmail.com>
2026-03-05 14:18:16 +0530

    debugging 2

--- src/main/java/com/example/login_demo/EmployeeRepository.java
+++ src/main/java/com/example/login_demo/EmployeeRepository.java
@@ -1,10 +1,8 @@
-package com.example.login_demo;
-import java.util.List;
+	package com.example.login_demo;
+	import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-    List<Employee> findByFnameContainingIgnoreCase(String fname);
-    }
+
+	public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+		List<Employee> findByFnameContainingIgnoreCase(String fname);
+		}
