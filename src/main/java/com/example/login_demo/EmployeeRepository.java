405e36fa1bf8ef424a906530501fd1a35c9d1197
Gopi <gopi27644@gmail.com>
2026-03-05 21:46:44 +0530

    Debugging 3

--- src/main/java/com/example/login_demo/EmployeeRepository.java
+++ src/main/java/com/example/login_demo/EmployeeRepository.java
@@ -5,4 +5,5 @@
 
 	public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 		List<Employee> findByFnameContainingIgnoreCase(String fname);
+		List<Employee> findByFnameIgnoreCase(String fname);
 		}
