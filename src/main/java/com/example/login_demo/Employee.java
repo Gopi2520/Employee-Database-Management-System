4d7a0836342b3cf2b4c3d14b6348557d2d74953a
Gopi <gopi27644@gmail.com>
2026-03-10 15:02:17 +0530

    Testing 1

--- src/main/java/com/example/login_demo/Employee.java
+++ src/main/java/com/example/login_demo/Employee.java
@@ -1,8 +1,6 @@
 package com.example.login_demo;
-
 import java.time.LocalDateTime;
 import java.util.Base64;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,9 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Transient;
-
 import org.hibernate.annotations.CreationTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -21,7 +17,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    
     private String fname;
     private String lname;
     private String contact;
